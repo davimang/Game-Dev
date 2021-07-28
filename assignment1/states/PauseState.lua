825a54b49f589e8ea88628e6e8005842b3872b30
David <moronitd@mcmaster.ca>
2021-07-28 19:11:06 -0400

    flappy bird

---
+++ assignment1/states/PauseState.lua
@@ -0,0 +1,18 @@
+-- PauseState = Class{__includes = PlayState}
+
+-- function PauseState:update(dt)
+--     scrolling = false
+    
+    
+   
+--     if  love.keyboard.wasPressed('w') then
+        
+--         sounds['music']:play()
+--         scrolling = true
+--     end
+
+
+-- end
+
+
+
