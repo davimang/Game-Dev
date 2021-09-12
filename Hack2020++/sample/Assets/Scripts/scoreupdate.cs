42d7b4ecc1e519489a92cc596748ef9bee7b13ab
davimang <davidmor956@gmail.com>
2021-09-12 11:09:50 -0400

    Hackathon 2020

---
+++ Hack2020++/sample/Assets/Scripts/scoreupdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scoreupdate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int score = 0;
+    Text currentScore;
+    void Start()
+    {
+      currentScore =  GetComponent<Text> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScore.text = "Score: " + score;
+    }
+}
